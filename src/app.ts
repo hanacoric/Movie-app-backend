10ecee8248ff94293aa190544e1c0110e15d80db
hanacoric <hancor01@easv365.dk>
2025-05-05 21:05:12 +0200

    CORS update

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
