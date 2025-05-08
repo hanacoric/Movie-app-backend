8eff28397065bd94f5abcc9905df145f4f79b559
hanacoric <hancor01@easv365.dk>
2025-05-09 00:37:57 +0200

    test

--- src/utils/verifyRecaptcha.ts
+++ src/utils/verifyRecaptcha.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const verifyRecaptcha = async (token: string): Promise<boolean> => {
   try {
-    const secret = process.env.RECAPTCHA_SECRET_KEY!;
+    const secret = process.env.VITE_RECAPTCHA_SECRET_KEY!;
     const response = await axios.post<{ success: boolean }>(
       `https://www.google.com/recaptcha/api/siteverify`,
       null,
