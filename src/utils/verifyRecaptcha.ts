f0c909f5e21e2d046b4a318cdcb61a45f5c17696
hanacoric <hancor01@easv365.dk>
2025-05-14 18:29:19 +0200

    final commit

--- src/utils/verifyRecaptcha.ts
+++ src/utils/verifyRecaptcha.ts
@@ -6,7 +6,7 @@ export const verifyRecaptcha = async (token: string): Promise<boolean> => {
   }
 
   try {
-    const secret = process.env.VITE_RECAPTCHA_SECRET_KEY!;
+    const secret = process.env.RECAPTCHA_SECRET_KEY!;
     const response = await axios.post<{ success: boolean }>(
       `https://www.google.com/recaptcha/api/siteverify`,
       null,
